21f17d0adc80cce52616190c2c9a3b88c0f481c1
IPontakI <valik_bilokin@ukr.net>
2024-06-04 08:17:21 +0300

    servApp

---
+++ goLang/internal/app/task_service.go
@@ -0,0 +1,75 @@
+package app
+
+import (
+	"log"
+
+	"github.com/BohdanBoriak/boilerplate-go-back/internal/domain"
+	"github.com/BohdanBoriak/boilerplate-go-back/internal/infra/database"
+)
+
+type TaskService interface {
+	Save(t domain.Task) (domain.Task, error)
+	FindByUserId(uId uint64) ([]domain.Task, error)
+	FindByTaskId(taskId uint64) (domain.Task, error)
+	Update(t domain.Task) (domain.Task, error)
+	DeleteByTaskId(taskId uint64) error
+}
+
+type taskService struct {
+	taskRepo database.TaskRepository
+}
+
+func NewTaskService(tr database.TaskRepository) TaskService {
+	return taskService{
+		taskRepo: tr,
+	}
+}
+
+func (s taskService) Save(t domain.Task) (domain.Task, error) {
+	task, err := s.taskRepo.Save(t)
+	if err != nil {
+		log.Printf("TaskService -> Save: %s", err)
+		return domain.Task{}, err
+	}
+	return task, nil
+}
+
+func (s taskService) FindByUserId(uId uint64) ([]domain.Task, error) {
+	tasks, err := s.taskRepo.FindByUserId(uId)
+	if err != nil {
+		log.Printf("TaskService -> FindByUserId: %s", err)
+		return nil, err
+	}
+	return tasks, nil
+}
+
+func (s taskService) FindByTaskId(taskId uint64) (domain.Task, error) {
+	task, err := s.taskRepo.FindByTaskId(taskId)
+	if err != nil {
+		log.Printf("TaskService -> FindByTaskId: %s", err)
+		return domain.Task{}, err
+	}
+	return task, nil
+}
+
+func (s taskService) Update(t domain.Task) (domain.Task, error) {
+	task, err := s.taskRepo.Save(t)
+	if err != nil {
+		log.Printf("TaskService -> Update: %s", err)
+		return domain.Task{}, err
+	}
+	return task, nil
+}
+
+func (s taskService) DeleteByTaskId(taskId uint64) error {
+	task, err := s.taskRepo.FindByTaskId(taskId)
+	if err != nil {
+		log.Printf("TaskService -> DeleteByTaskId: %s", err)
+		return err
+	}
+	_, err = s.taskRepo.DeleteByTaskId(task)
+	if err != nil {
+		log.Printf("TaskService -> DeleteByTaskId: %s", err)
+	}
+	return err
+}
---
+++ goLang/internal/infra/http/controllers/task_controller.go
@@ -0,0 +1,113 @@
+package controllers
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/BohdanBoriak/boilerplate-go-back/internal/app"
+	"github.com/BohdanBoriak/boilerplate-go-back/internal/domain"
+	"github.com/BohdanBoriak/boilerplate-go-back/internal/infra/http/requests"
+	"github.com/BohdanBoriak/boilerplate-go-back/internal/infra/http/resources"
+)
+
+type TaskController struct {
+	taskService app.TaskService
+}
+
+func NewTaskController(ts app.TaskService) TaskController {
+	return TaskController{
+		taskService: ts,
+	}
+}
+
+func (c TaskController) Save() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		task, err := requests.Bind(r, requests.TaskRequest{}, domain.Task{})
+		if err != nil {
+			log.Printf("TaskController -> Save: %s", err)
+			BadRequest(w, err)
+			return
+		}
+
+		user := r.Context().Value(UserKey).(domain.User)
+		task.UserId = user.Id
+		task.Status = domain.NewTaskStatus
+		task, err = c.taskService.Save(task)
+		if err != nil {
+			log.Printf("TaskController -> Save: %s", err)
+			InternalServerError(w, err)
+			return
+		}
+
+		var tDto resources.TaskDto
+		tDto = tDto.DomainToDto(task)
+		Created(w, tDto)
+	}
+}
+
+func (c TaskController) FindByUserId() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		user := r.Context().Value(UserKey).(domain.User)
+		tasks, err := c.taskService.FindByUserId(user.Id)
+		if err != nil {
+			log.Printf("TaskController -> FindByUserId: %s", err)
+			InternalServerError(w, err)
+			return
+		}
+
+		var tsDto resources.TasksDto
+		tsDto = tsDto.DomainToDtoCollection(tasks)
+		Success(w, tsDto)
+	}
+}
+
+func (c TaskController) FindByTaskId(taskId uint64) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		task, err := c.taskService.FindByTaskId(taskId)
+		if err != nil {
+			log.Printf("TaskController -> FindByTaskId: %s", err)
+			InternalServerError(w, err)
+			return
+		}
+
+		var tDto resources.TaskDto
+		tDto = tDto.DomainToDto(task)
+		Success(w, tDto)
+	}
+}
+
+
+func (c TaskController) Update(taskId uint64) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		task, err := requests.Bind(r, requests.TaskRequest{}, domain.Task{})
+		if err != nil {
+			log.Printf("TaskController -> Update: %s", err)
+			BadRequest(w, err)
+			return
+		}
+
+		task.Id = taskId
+		updatedTask, err := c.taskService.Update(task)
+		if err != nil {
+			log.Printf("TaskController -> Update: %s", err)
+			InternalServerError(w, err)
+			return
+		}
+
+		var tDto resources.TaskDto
+		tDto = tDto.DomainToDto(updatedTask)
+		Success(w, tDto)
+	}
+}
+
+func (c TaskController) Delete(taskId uint64) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		err := c.taskService.DeleteByTaskId(taskId)
+		if err != nil {
+			log.Printf("TaskController -> Delete: %s", err)
+			InternalServerError(w, err)
+			return
+		}
+	}
+}
+
